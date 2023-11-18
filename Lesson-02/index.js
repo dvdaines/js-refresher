4b5e02539a561d20cbfaddf9bf7694be5cfa0245
dvdaines <ddaines@protonmail.com>
2023-11-18 15:37:17 -0600

    Lesson 2 Pt. 2  (Build Blackjack Game)

--- Lesson-02/index.js
+++ Lesson-02/index.js
@@ -1,33 +1,52 @@
-function randomInteger(min, max) {
+let cards = []
+let sum = 0
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function getRandomCard() {
+  let min = 2
+  let max = 11
   let range = max - min
   let result = Math.floor((range + 1) * Math.random() + min)
   return(result)
 }
 
-let firstCard = randomInteger(2, 11)
-let secondCard = randomInteger(2, 11)
-let sum = firstCard + secondCard
-let hasBlackjack = false
-let isAlive = true
-let message = ""
-
-console.log(sum)
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-  message = "Blackjack! 🥳"
-  hasBlackjack = true
-} else {
-  message = "BTFO! 😂"
-  isAlive = false
+function startGame() {
+  cards.push(getRandomCard(2, 11))
+  cards.push(getRandomCard(2, 11))
+  renderGame()
 }
 
-console.log(message)
+function renderGame() {
+  let newSum = 0
+  let cardsString = "Cards: "
+  cards.forEach(function(card) {
+    newSum += card
+    cardsString += card + " "
+  })
+  sum = newSum
+  cardsEl.textContent = "Cards: " + cardsString
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "Blackjack!"
+    hasBlackjack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
 
-// false
-// true
-// false
-// false
-// true
-// true
-// false
+
+function newCard() {
+  cards.push(getRandomCard(2, 11))
+  renderGame()
+}
+
